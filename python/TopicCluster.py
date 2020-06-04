107340270174b2bd8d0692c8a76555bcae06832d
hochen-wish <hochen@wish.com>
2020-06-04 16:16:12 -0700

    topic analysis from labels on word clusters

--- python/TopicCluster.py
+++ python/TopicCluster.py
@@ -7,29 +7,22 @@ import nltk
 MODEL_FILE = 'models/731458419_model.pkl'#'models/737684276_Gadgets_topic_model.pkl'#
 model_ak: Word2Vec = pickle.load(open(MODEL_FILE, 'rb'))
 
-"""input_file = "/Users/hochen/Documents/Projects/prd-review/737684276_Gadgets_topic_filtered - 737684276_Gadgets_topic_filtered.csv"
-rows = -1
-df = loadDataFromCsv(input_file, rows=rows)"""
-
-MODEL_FILE2 = 'models/737684276_Gadgets_topic_model.pkl'#
-model_ak2: Word2Vec = pickle.load(open(MODEL_FILE2, 'rb'))
-
-word_list:list = list(model_ak2.wv.vocab)[:15000]
+word_list:list = list(model_ak.wv.vocab)[:15000]
 
 X=[]
 for w in word_list:
     if w in model_ak.wv.vocab:
         X.append(model_ak[w])
 
-NUM_CLUSTERS=350
+NUM_CLUSTERS=500
 kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance, repeats=25)
 print("start clustering ...")
 assigned_clusters = kclusterer.cluster(X, assign_clusters=True)
 #print (assigned_clusters)
 
-pickle.dump(kclusterer, open("models/737684276_Gadgets_topic_cluster.pkl", 'wb'))
+pickle.dump(kclusterer, open("models/731458419_word_cluster.pkl", 'wb'))
 
-with open("models/737684276_Gadgets_topic_word_cluster.csv", 'w') as f:
+with open("models/731458419_word_cluster.csv", 'w') as f:
     for i in range(NUM_CLUSTERS):
         writer = csv.writer(f)
 
